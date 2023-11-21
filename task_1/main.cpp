cab6a619f750195ee227d87c9b159521590f4f87
KrestinaSD <krestina.sofya@gmail.com>
2023-11-21 21:19:51 +0300

    all files

---
+++ task_1/main.cpp
@@ -0,0 +1,57 @@
+#include "modAlphaCipher.cpp"
+#include <locale>
+
+bool isValid(const string& s)
+{
+    std::locale loc("ru_RU.UTF-8");
+    std::wstring_convert<std::codecvt_utf8<wchar_t>, wchar_t> codec;
+    std::wstring ws = codec.from_bytes(s);
+    std::wstring numAlpha = L"АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+    for(unsigned int i = 0; i < ws.size(); i++) {
+        if (numAlpha.find(ws[i]) == std::wstring::npos) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    string key;
+    string text;
+    unsigned op;
+    cout << "Cipher ready. Input key: ";
+    cin >> key;
+
+    if (!isValid(key)) {
+        cerr << "Key not valid\n";
+        return 1;
+    }
+
+    cout << "Key loaded\n";
+    modAlphaCipher cipher(key);
+
+    do {
+        cout << "Cipher ready. Input operation (0-exit, 1-encrypt, 2-decrypt): ";
+        cin >> op;
+
+        if (op > 2) {
+            cout << "Illegal operation\n";
+        } else if (op > 0) {
+            cout << "Cipher ready. Input text: ";
+            cin.ignore();
+            getline(cin, text);
+
+            if (isValid(text)) {
+                if (op==1) {
+                    cout << "Encrypted text: " << cipher.encrypt(text) << endl;
+                } else {
+                    cout << "Decrypted text: " << cipher.decrypt(text) << endl;
+                }
+            } else {
+                cout << "Operation aborted: invalid text\n";
+            }
+        }
+    } while (op != 0);
+    return 0;
+}
---
+++ task_2/main.cpp
@@ -0,0 +1,50 @@
+#include  "coder.cpp"
+#include <locale>
+using namespace std;
+
+bool isValid(const wstring& s)
+{
+
+    for (auto c:s)
+        if (!iswalpha(c) || !iswupper(c))
+            return false;
+    return true;
+}
+
+int main()
+{
+    locale loc("ru_RU.UTF-8");
+    locale::global(loc);
+    int key;
+    wstring text;
+    int op;
+    wcout<<L"<Крипер> готов. Введи ключ (целое число): ";
+    wcin>>key;
+    if (!wcin.good()) {
+        wcerr<<L"ключ недействителен.\n";
+        return 0;
+    }
+
+    coder cipher(key);
+    do {
+        wcout<<L"<Крипер> готов. Введи операцию (0-завершить, 1-зашифровать, 2-расшифровать): ";
+        wcin>>op;
+        if (op != 0 and op != 1 and op != 2) {
+            wcout<<L"НЕзаконная операция!\n";
+        } else if (op >0) {
+            wcout<<L"<Крипер> готов. Введи текст(без пробелов и капсом): ";
+            wcin.ignore(); 
+            getline(wcin, text);
+            if (isValid(text)) {
+                if (op == 1) {
+                    wcout<<L"Зашифрованный текст: "<<cipher.encrypt(text)<<endl;
+                } else if (op == 2){
+                    wcout<<L"Расшифрованный текст: "<<cipher.decrypt(text)<<endl;
+                }
+            } else {
+                wcout<<L"Операция прервана.... неверный текст....\n";
+            }
+        }
+    } while (op != 0);
+    return 0;
+}
